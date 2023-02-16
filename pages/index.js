b0087689e82ccb6ae6892d83a1c77edf0c73a986
internetdrew <andrew@internetdrew.com>
2023-02-16 10:53:39 -0500

    improve mobile text sizing

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
-import Head from 'next/head';
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <>
       <section className='bg-[#ede0d4] h-screen overflow-y-hidden relative'>
         <div className='text-center py-10'>
-          <h1 className='text-3xl'>ListCleanser</h1>
-          <p className='text-2xl'>
+          <h1 className='text-xl sm:text-2xl'>ListCleanser</h1>
+          <p className='text-lg sm:text-xl'>
             Make your favorite cuss-up-music playlists family-friendly(ish).
           </p>
         </div>
         <div className='text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
           <a
             href=''
-            className='p-2 border-solid border-2 border-black text-xl shadow-[4px_4px_2px_1px_rgb(0,0,0,1)]'
+            className='p-2 border-solid border-2 border-black text-sm sm:text-lg shadow-[2px_2px_2px_1px_rgb(0,0,0,1)]'
           >
             Login to Spotify
           </a>
