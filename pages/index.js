37bac150fed40d7a424480176a6548e5f1518eba
internetdrew <andrew@internetdrew.com>
2023-02-16 09:21:26 -0500

    add background color

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-orange-100 h-screen'>
+      <Head>
+        <title>ListCleanser</title>
+      </Head>
       <h1>Hello world</h1>
     </div>
   );
